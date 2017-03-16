8f6edb5c7d5e9e33dd5e1c6ca4327c60c194179a
Thaddee Wiseur <twiseur@gmail.com>
2017-03-15 22:29:06 -0700

    Added homework 4

---
+++ HelloWorld/NavigatorWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for NavigatorWindow.xaml
+    /// </summary>
+    public partial class NavigatorWindow : Window
+    {
+        public NavigatorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void uxGoButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Pass the fileName to the helper class
+            var processStartInfo = new ProcessStartInfo(uxURLTextBox.Text);
+
+            // Start a new process
+            Process.Start(processStartInfo);
+        }
+
+        private void uxURLTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ZipCodeChecker/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// CSHP220 Homework 4
+// Author: Thaddee Wiseur
+
+namespace ZipCodeChecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string zipCodePattern = "(^([0-9][0-9][0-9][0-9][0-9](-[0-9][0-9][0-9][0-9])?)$)|(^[A-Z][0-9][A-Z][0-9][A-Z][0-9]$)";
+        private Regex regex;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            regex = new Regex(zipCodePattern);
+            uxZipCodeTextBox.Focus();
+        }
+
+        private void updateSubmitButton ()
+        {
+            uxSubmitButton.IsEnabled = regex.IsMatch(uxZipCodeTextBox.Text);
+            if (uxSubmitButton.IsEnabled)
+            {
+                uxZipCodeTextBox.Foreground = new SolidColorBrush(Colors.ForestGreen);
+            }
+            else
+            {
+                uxZipCodeTextBox.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+        private void uxZipCodeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateSubmitButton();
+        }
+
+        private void uxSubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Valid ZIP code!");
+        }
+    }
+}
